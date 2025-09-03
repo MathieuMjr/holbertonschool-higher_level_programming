ad290dfa65ce762e968a82d23d9b5ddb452039d3
Mathieu <mathieu.mjr@gmail.com>
2025-09-03 16:45:27 +0200

    fixed indent

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,11 @@
 import sys
 
 if __name__ == '__main__':
-	if len(sys.argv) == 2:
-		print("{} argument:".format(len(sys.argv) - 1))
-	elif len(sys.argv) == 1:
-		print("0 arguments.")
-	else:
-		print("{} arguments:".format(len(sys.argv) - 1))
-	for i in range(1, len(sys.argv)):
-		print("{}: {}".format(i, sys.argv[i]))
+    if len(sys.argv) == 2:
+        print("{} argument:".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i]))
