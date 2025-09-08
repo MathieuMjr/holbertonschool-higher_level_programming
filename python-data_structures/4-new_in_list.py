eeeb8a7cac5fc27fda4ddd780c7f6d4b65217755
Mathieu <mathieu.mjr@gmail.com>
2025-09-08 14:59:32 +0200

    function return a modified copy of a list with comprehension

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    return [element if idx == element else my_list[i]
+            for i in range(0, len(my_list))]
+
+#    new_list = []
+#
+#    for i in range(0, len(my_list)):
+#        if i != idx:
+#            new_list.append(my_list[i])
+#        else:
+#            new_list.append(element)
+#    return new_list
