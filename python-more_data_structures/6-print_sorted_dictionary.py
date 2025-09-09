bbb2b37d6fadc1c012a68963f46ec585080a464d
Mathieu <mathieu.mjr@gmail.com>
2025-09-09 22:34:03 +0200

    commented an alternative solution

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,3 +2,8 @@
 def print_sorted_dictionary(a_dictionary):
     for key, value in sorted(a_dictionary.items()):
         print("{}: {}".format(key, value))
+
+# alternative :
+# for key in sorted(a_dictionary):
+#    print("{}: {}".format(key, a_dictionary[key]))
+# tuples are not used this time, so no need to .items
