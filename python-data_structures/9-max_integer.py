bccd0f20ac79e3148835fafecaecc15824e60e6e
Mathieu <mathieu.mjr@gmail.com>
2025-09-09 09:26:57 +0200

    fixed : removed max built in (not allowed)

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,4 +3,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        return (max(my_list))
+        maximum = my_list[0]
+        for x in my_list:
+            if x > maximum:
+                maximum = x
+    return (maximum)
