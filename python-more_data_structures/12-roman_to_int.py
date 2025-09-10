de9b5bd328b414506faa3350d4333789842d9c41
Mathieu <mathieu.mjr@gmail.com>
2025-09-10 15:22:13 +0200

    try to fix case where arg given is not a string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = 0
 
-    if not roman_string and isinstance(roman_string, str):
+    if not roman_string and not isinstance(roman_string, str):
         return None
     for i in range(0, len(roman_string)):
         if roman_string[i] == "I" and (
