ac6545288fbc3e22ccb3977d894e9a266cd83a8f
Mathieu <mathieu.mjr@gmail.com>
2025-09-10 15:18:12 +0200

    fixed cases where I = -1

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = 0
 
     if not roman_string:
         return None
     for i in range(0, len(roman_string)):
-        if roman_string[i] == 'I' and i == 0 and i + 1 < len(roman_string):
+        if roman_string[i] == "I" and (
+            i + 1 < len(roman_string)
+            and (roman_string[i + 1] == "X" or roman_string[i + 1] == "V")
+        ):
             number -= 1
         else:
             number += roman[roman_string[i]]
