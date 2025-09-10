63af2eae846259b65215596d0fb8cf885cd40a98
Mathieu <mathieu.mjr@gmail.com>
2025-09-10 10:33:35 +0200

    fixed : name is returned and not value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    name = ""
     max = 0
     if a_dictionary is None:
         return None
     for key in a_dictionary:
         if a_dictionary[key] > max:
             max = a_dictionary[key]
-            return max
+            name = key
+            return name
