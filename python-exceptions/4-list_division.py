d34e7ea8e26e8c9efa8c3c93b044c375fb905d6f
Mathieu <mathieu.mjr@gmail.com>
2025-09-11 14:03:16 +0200

    function that divid element of 2 lists, if an error occurs, error message is printed and 0 is the result

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for i in range(0, list_length):
+        try:
+            res.append(my_list_1[i] / my_list_2[i])
+        except IndexError:
+            res.append(0)
+            print("out of range")
+        except ZeroDivisionError:
+            res.append(0)
+            print("division by 0")
+        except TypeError:
+            res.append(0)
+            print("wrong type")
+    return res
