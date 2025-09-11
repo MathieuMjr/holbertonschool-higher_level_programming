c0c47a2cdfae22f1e202eb354140cf57bda32743
Mathieu <mathieu.mjr@gmail.com>
2025-09-11 11:00:53 +0200

    removed incorrect condition to raise error

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value is not isinstance(value, int) or value is None:
-            raise ValueError
         print("{:d}".format(value))
         return True
     except (ValueError, NameError, TypeError):
