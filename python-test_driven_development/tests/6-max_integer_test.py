bc495e9cc8f2499eda855fe91d40e9a460c8e7b7
Mathieu <mathieu.mjr@gmail.com>
2025-09-12 17:09:51 +0200

    fixed cases max in the middle, list of one element and empty list

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,3 +6,6 @@ class TestMaxInt(unittest.TestCase):
     def test_value(self):
         self.assertAlmostEqual(max_integer([1, 2, 3]), 3)
         self.assertAlmostEqual(max_integer([-1, -2, -3]), -1)
+        self.assertAlmostEqual(max_integer([1, 4, 3]), 4)
+        self.assertAlmostEqual(max_integer([1]), 1)
+        self.assertAlmostEqual(max_integer([]), None)
