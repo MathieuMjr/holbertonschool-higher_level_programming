00b290eacfd3bda2d79d975a705c03a81172865e
Mathieu <mathieu.mjr@gmail.com>
2025-09-12 16:19:13 +0200

    fixed : no more space at beginning of newlines

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -11,8 +11,10 @@ def text_indentation(text):
     """function that print text and 2 newlines after "." ":" and "?" chars"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for char in text:
-        print(char, end="")
-        if char in [".", ":", "?"]:
+    for idx in range(0, len(text)):
+        if text[idx] == " " and text[idx - 1] in [".", ":", "?"]:
+            continue
+        print(text[idx], end="")
+        if text[idx] in [".", ":", "?"]:
             print()
             print()
