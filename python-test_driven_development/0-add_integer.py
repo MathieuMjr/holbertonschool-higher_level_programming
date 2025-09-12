dd8e7d31c3a9ec855fad1cd43be984afbe639328
Mathieu <mathieu.mjr@gmail.com>
2025-09-12 10:49:42 +0200

    documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
+"""
+0-add_integer.py
+
+This module contains a fonction that add to integers.
+Floats can be used and will be typecasted into ints.
+"""
+
+
 def add_integer(a, b=98):
+    """add a and b, checking if they are ints or floats"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
