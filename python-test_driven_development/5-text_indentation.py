1b640c5cce2b1a4fd95a035f504bdac6b63f4de8
Mathieu <mathieu.mjr@gmail.com>
2025-09-12 16:31:30 +0200

    fixed use of counter

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,7 +15,7 @@ def text_indentation(text):
     for idx in range(0, len(text)):
         if text[idx] != " " and text[idx - 1] in [".", ":", "?"]:
             counter = 0
-        if text[idx] == " " and text[idx - 1] in [".", ":", "?"]:
+        if text[idx] == " " and counter != 0:
             continue
         print(text[idx], end="")
         if text[idx] in [".", ":", "?"]:
