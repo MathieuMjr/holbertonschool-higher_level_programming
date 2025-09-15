d71b1f7b08ecd5d884bf4db6f2333da545eaa33c
Mathieu <mathieu.mjr@gmail.com>
2025-09-15 15:42:52 +0200

    Gave a size attribute to my class square

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""Ce module contient la classe Square qui représente un carré."""
+
+class Square:
+    """
+    Cette classe représente un carré.
+
+    """
+
+    def __init__(self, size=None):
+        """ 
+        Initialise un carré
+        
+        Args:
+            size (int): Taille du carré.
+        """
+        if size is None or not isinstance(size, int):
+            raise AttributeError("'Square' object has no attribute 'size'")
+        self.__size = size
