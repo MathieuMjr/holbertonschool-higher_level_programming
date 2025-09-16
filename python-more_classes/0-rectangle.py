9d37eb29e8766b44a8b1c9ae791449fef3d251e7
Mathieu <mathieu.mjr@gmail.com>
2025-09-16 15:00:34 +0200

    created an empty class that represent a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class representing a rectangle,
+its attributes and its methods
+"""
+
+
+class Rectangle:
+    """
+    This class represent a rectangle
+    """
+    pass
