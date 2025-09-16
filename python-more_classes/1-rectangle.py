d289ec6afdd4211af69ebbdd84d187c4f8cb912d
Mathieu <mathieu.mjr@gmail.com>
2025-09-16 15:29:56 +0200

    added : width and height private instance attributes + setters and getters

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class representing a rectangle,
+its attributes and its methods
+"""
+
+
+class Rectangle:
+    """
+    This class represent a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+        else:
+            raise TypeError("width must be an integer")
+        self.__height = value
