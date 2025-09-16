38f0f1185f7e9702395504bb6732477eb80705a3
Mathieu <mathieu.mjr@gmail.com>
2025-09-16 10:35:10 +0200

    added a method that print the square

---
+++ python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""Ce module contient la classe Square qui représente un carré."""
+
+
+class Square:
+    """
+    Cette classe représente un carré.
+
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialise un carré
+
+        Args:
+            size (int): Taille du carré.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter: permet d'accéder à a valeur de __size
+        via "size" """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter: permet de modifier la valeur de __size
+        mais sous certaines conditions ! et via le mot "size"
+        et pas __size directement """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calcul la surface du carré
+        """
+        return pow(self.__size, 2)
+
+    def my_print(self):
+        """
+        Dessine le carré avec des symboles "#"
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for ligne in range(0, self.__size):
+                for element in range(0, self.__size):
+                    print("#", end="")
+                print()
