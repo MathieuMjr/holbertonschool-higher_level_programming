af32e05e6a3bee2610917d144360ef9e70660343
Mathieu <mathieu.mjr@gmail.com>
2025-09-22 17:59:03 +0200

    added exception rising

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains a class
+"""
+
+
+class BaseGeometry:
+    """
+    This is an empty class
+    """
+    def area(self):
+        """
+        Function not defined yet
+        Raises an execption
+        """
+        raise Exception("area() is not implemented")
