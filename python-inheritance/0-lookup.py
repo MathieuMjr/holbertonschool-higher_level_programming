f2640584d2e40009246da2a9795129eef07cc963
Mathieu <mathieu.mjr@gmail.com>
2025-09-22 14:33:56 +0200

    function that return a list of attribute and methods of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a function that return
+attributes and methodes of an object"""
+
+
+def lookup(obj):
+    """Function that returns list of attributes
+    and method of an object"""
+    return dir(obj)
