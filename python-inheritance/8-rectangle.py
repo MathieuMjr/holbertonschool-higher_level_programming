226c7c02daa0bcfe0de783c14bc8488882dc7225
Mathieu <mathieu.mjr@gmail.com>
2025-09-23 10:59:37 +0200

    fixed : BaseGeomtry class is imported from file 7-base_geomtry so it please the checker

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,29 +1,11 @@
 #!/usr/bin/python3
 """
-This module contains a class
+This module contains a subclass that inherit from*
+Base Geometry
 """
 
 
-class BaseGeometry:
-    """
-    This is an empty class
-    """
-    def area(self):
-        """
-        Function not defined yet
-        Raises an execption
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Function that says if a value is an integer.
-        Name's just there to be printed in erro message.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
