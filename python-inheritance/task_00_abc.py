d791bc22a8412caff7357190d65b39c8792a3d6f
Mathieu <mathieu.mjr@gmail.com>
2025-09-23 16:46:36 +0200

    create ABC Animal with abstractmethod "sound" + dog and cat classes that can makes sounds

---
+++ python-inheritance/task_00_abc.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+This module contains several classes :
+- Animal - abstract class that defines
+the others.
+- Others types of animals that inherits from
+Animals attributes
+"""
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    This class defines what any animal
+    can do. For now, just a sound.
+    """
+
+    @abstractmethod
+    def sound():
+        pass
+
+
+class Dog(Animal):
+    """
+    This class defines what a dog is and can do.
+    Since its an animal, it as sound method.
+    Dog's sound is bark, so we defined it
+    this way
+    """
+
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+    """
+    This class defines what a cat is and can do.
+    It's an animal so just like dog
+    it's a subclass of Animal.
+    Cat's sound is "Meow".
+    """
+
+    def sound(self):
+        return "Meow"
