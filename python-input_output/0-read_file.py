052c15cf5a841e321e63de41d8e194e120eb347d
Mathieu <mathieu.mjr@gmail.com>
2025-09-29 10:28:02 +0200

    description

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+This module contains a function that reads files.
+"""
+
 
 def read_file(filename=""):
+    """
+    Reads the content of a file and print it
+    """
     with open('my_file_0.txt', 'r', encoding="utf-8") as file:
         read_file = file.read()
     print(read_file)
