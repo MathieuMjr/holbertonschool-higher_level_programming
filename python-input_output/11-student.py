0605d294011f462c5dbbeb05ab1beb47678f3f27
Mathieu <mathieu.mjr@gmail.com>
2025-09-30 18:35:07 +0200

    new method

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -30,5 +30,6 @@ class Student:
         return self.__dict__
 
     def reload_from_json(self, json):
-        self.__dict__ = json
-        return self.__dict__
+        for key, value in json.items():
+            setattr(self, key, value)
+        return self
