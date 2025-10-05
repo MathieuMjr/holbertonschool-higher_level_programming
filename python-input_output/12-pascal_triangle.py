6e64a43eb17e917fdf905f3c238cad54a786fd64
Mathieu <mathieu.mjr@gmail.com>
2025-10-06 00:02:54 +0200

    function that draw a pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+def pascal_triangle(n):
+    my_list = []
+    for i in range(1, n + 1):
+        new_list = []
+        if i <= 2:
+            for j in range(0, i):
+                new_list.append(1)
+            my_list.append(new_list)
+        else:
+            for j in range(0, i):
+                try:
+                    if j == 0:
+                        new_list.append(1)
+                    else:
+                        new_list.append(my_list[i - 2][j] +
+                                        my_list[i - 2][j - 1])
+                except Exception:
+                    new_list.append(1)
+            my_list.append(new_list)
+    return my_list
+
+
+# correction chat gpt :
+# def pascal_triangle(n):
+#    triangle = []
+#   for i in range(n):
+#        row = [1] * (i + 1)
+#        for j in range(1, i):
+#            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]  # ③
+#        triangle.append(row)
+#    return triangle
