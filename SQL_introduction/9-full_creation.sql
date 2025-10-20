8294cd67b3a4b1cabd603f2fde7e5e6df4270925
Mathieu <mathieu.mjr@gmail.com>
2025-10-20 17:29:25 +0200

    fixed : bad score for Alex

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS `second_table` (
 );
 
 INSERT INTO `second_table`
-VALUES (1, "John", 10), (2, "Alex", 2), (3, "Bob", 14), (4, "George", 8);
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
