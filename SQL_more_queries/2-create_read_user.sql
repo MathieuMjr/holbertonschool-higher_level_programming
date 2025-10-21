032ba71b48b0a7c3bf4a3002ab530b14e2f09212
Mathieu <mathieu.mjr@gmail.com>
2025-10-21 14:36:04 +0200

    removed '' around database name

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- script that gives read permission on a user on a specific database
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';*
 GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
