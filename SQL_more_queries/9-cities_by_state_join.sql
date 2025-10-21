6d5cc8b6cfe403d616406ff9a6d57474c626e96e
Mathieu <mathieu.mjr@gmail.com>
2025-10-21 15:55:51 +0200

    script that list all cities with states name

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- script that list all cities with states name
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
+FROM `cities`
+INNER JOIN `states`
+    ON `states`.`id` =  `cities`.`state_id`
+ORDER BY `cities`.`id` ASC
