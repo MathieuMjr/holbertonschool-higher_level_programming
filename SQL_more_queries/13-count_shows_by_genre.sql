655022e6d62376aa325527f8d6a78f597b22ae54
Mathieu <mathieu.mjr@gmail.com>
2025-10-21 21:37:48 +0200

    try a fix in count alias

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Script that return the of shows by genre
-SELECT `tv_genres`.`name`, COUNT(`tv_show_genres`.`show_id`) AS 'number_of_shows'
+SELECT `tv_genres`.`name`, COUNT(`tv_show_genres`.`show_id`) AS `number_of_shows`
 FROM tv_genres
 INNER JOIN tv_show_genres
     ON tv_genres.id = tv_show_genres.genre_id
