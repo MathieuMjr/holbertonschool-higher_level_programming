dae2569c9aa6e188ff3fa3799ae183834d5b1267
Mathieu <mathieu.mjr@gmail.com>
2025-10-22 18:10:01 +0200

    added port number

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,29 +2,26 @@
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    if len(sys.argv) == 4:
-        user = sys.argv[1]
-        mdp = sys.argv[2]
-        db_name = sys.argv[3]
+    user = sys.argv[1]
+    mdp = sys.argv[2]
+    db_name = sys.argv[3]
 
-        # function to connect to a MySQL database
-        db = MySQLdb.connect(
-            host='localhost', user=user, passwd=mdp, db=db_name)
-        cur = db.cursor()
-        # créer un curseur - permet d'avoir plusieurs environnement
-        # sur la même connexion à la DB ?
+    # function to connect to a MySQL database
+    db = MySQLdb.connect(
+        host='localhost', user=user, passwd=mdp, db=db_name, port=3306)
+    cur = db.cursor()
+    # créer un curseur - permet d'avoir plusieurs environnement
+    # sur la même connexion à la DB ?
 
-        cur.execute("SELECT * FROM states ORDER BY id")  # envoie une requête
-        rows = cur.fetchall()
-        # récupère les lignes renvoyées par la base
-        # et les stock en liste de tuples
-        # on peut récupérer une ligne à la fois ou un nombre n de lignes
-        # avec fetchone() et fetchmany(n)
+    cur.execute("SELECT * FROM states ORDER BY id")  # envoie une requête
+    rows = cur.fetchall()
+    # récupère les lignes renvoyées par la base
+    # et les stock en liste de tuples
+    # on peut récupérer une ligne à la fois ou un nombre n de lignes
+    # avec fetchone() et fetchmany(n)
 
-        for element in rows:
-            print(element)
+    for element in rows:
+        print(element)
 
-        cur.close()
-        db.close()
-    else:
-        sys.exit(1)
+    cur.close()
+    db.close()
