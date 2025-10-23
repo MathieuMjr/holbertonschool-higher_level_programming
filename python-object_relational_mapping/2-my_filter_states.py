a898dfb624f1258b0a71a41b2ec8319be98f278f
Mathieu <mathieu.mjr@gmail.com>
2025-10-23 12:10:47 +0200

    added quotes + missing space

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     # sur la même connexion à la DB ?
 
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY {}"
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' "
         "ORDER BY id ASC".format(name_arg))
     # envoie une requête
     rows = cur.fetchall()
