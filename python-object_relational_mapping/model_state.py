cae6853ed6ff8c66ae3d73fdb523c6490ae25acf
Mathieu <mathieu.mjr@gmail.com>
2025-10-23 18:59:04 +0200

    docstring

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+"""
+This module contains a schema for database hbtn_0e_6_usa
+"""
 
 Base = declarative_base()
 
