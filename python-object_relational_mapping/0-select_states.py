b1ba3b05f9ade41e02c2da4bb1b9ac59ccd888d5
Mathieu <mathieu.mjr@gmail.com>
2025-10-23 10:32:49 +0200

    deleted an empty line + fixedd indent in db

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -18,12 +18,11 @@ if __name__ == "__main__":
         user=username,
         passwd=password,
         db=database,
-        )
+    )
 
     cur = db.cursor()
 
     cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-
     rows = cur.fetchall()
 
     for row in rows:
