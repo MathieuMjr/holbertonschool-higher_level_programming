dc5a4e32da3e38e2eed9b1301973a78bbb958acd
Mathieu <mathieu.mjr@gmail.com>
2025-10-24 15:56:33 +0200

    script that a a new object to database

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module contain query to add a new
+instance of a class State to db
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()  # creation d'une sessions
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    res = session.query(State).filter(State.name == new_state.name).first()
+    if res:
+        print(f"{res.id}")
+    else:
+        print("Not found")
+
+    session.close()
+    engine.dispose()
