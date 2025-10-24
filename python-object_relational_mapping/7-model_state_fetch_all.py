e7287c35cefb2024fd9d4915d17c6ea08a088359
Mathieu <mathieu.mjr@gmail.com>
2025-10-24 11:38:19 +0200

    script that make a select query on state table

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contain query for getting all
+instances of a class State
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()  # creation d'une sessions
+
+    # réalisation d'une query Select id, name from state order by id
+    for instance in session.query(State).order_by(State.id):
+        print(f"{instance.id}: {instance.name}")
+
+    session.close()
+    engine.dispose()
