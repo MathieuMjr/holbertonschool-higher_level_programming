97c2a1700396ec29220880860305d253d3138b5d
Mathieu <mathieu.mjr@gmail.com>
2025-10-24 15:00:45 +0200

    script that find all states with 'a' in their name

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module contain query for getting all
+instances of a class State
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()  # creation d'une sessions
+
+    # réalisation d'une query Select id, name from state order by id où id =1
+    # ici, pas besoin de boucle car on récupère le 1er résultat
+    res = session.query(State).filter(State.name.like('%a%'))
+
+    if res:
+        for instance in res:
+            print(f"{instance.id}: {instance.name}")
+    else:
+        print("Nothing")
+
+    session.close()
+    engine.dispose()
