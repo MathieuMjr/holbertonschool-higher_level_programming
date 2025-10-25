c3807caceb3409ddbaa3b0d178a4d59a40cb71cb
Mathieu <mathieu.mjr@gmail.com>
2025-10-25 20:44:54 +0200

    script that fetch cities and print their state and name via state id

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+This module contain query to add a new
+instance of a class State to db
+"""
+import sys
+from model_state import Base
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()  # creation d'une sessions
+
+res = session.query(City).order_by(City.id)
+if res:
+    for instance in res:
+        print((f"{instance.state.name}: ({instance.id}) {instance.name}"))
+
+session.close()
+engine.dispose()
