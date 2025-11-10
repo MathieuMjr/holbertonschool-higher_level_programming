3c0153df9713edcee32d3f258653f98edee4ff80
Mathieu <mathieu.mjr@gmail.com>
2025-11-10 17:17:55 +0100

    'arguments' if more than one

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 const args = process.argv;
-if (args[2]) {
+if (args[3]) {
+  console.log('Arguments found');
+} else if (args[2]) {
   console.log('Argument found');
 } else {
   console.log('No argument found');
