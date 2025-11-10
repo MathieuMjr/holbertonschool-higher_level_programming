ed95e050c66cc228ea81bfcb7b44d124782b28ae
Mathieu <mathieu.mjr@gmail.com>
2025-11-10 17:02:03 +0100

    fixed S uppercase in JavaScript

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const text of myVar) {
   console.log(text);
