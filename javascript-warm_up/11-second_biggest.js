a0a4878df340864cbebaeae9f7240332acae8296
Mathieu <mathieu.mjr@gmail.com>
2025-11-11 11:47:37 +0100

    work in progress

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
-
+let array = []
 let counter = 2;
 let biggest = 0;
+let counter2 = 0;
+let scdbiggest = 0;
 
 while (process.argv[counter] !== undefined) {
+  array.push(parseInt(process.argv[counter]));
   if (biggest < parseInt(process.argv[counter])) {
     biggest = parseInt(process.argv[counter]);
   }
   counter++;
+}
+
+while (counter2 !== counter - 2) {
+    if (scdbigget)
 }
 if (counter > 3) {
   console.log(biggest);
