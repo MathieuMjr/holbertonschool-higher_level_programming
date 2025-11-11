642bf5a7b9cc72ccf186ee373d7ed10490bd46a3
Mathieu <mathieu.mjr@gmail.com>
2025-11-11 12:18:41 +0100

    removed comment so the checker is not triggered

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,10 +2,6 @@
 
 const arg = process.argv[2];
 const number = parseInt(arg);
-/* +var permet de transformer une string en nombre;
-Number(...) aussi et conserve les chiffres après la virgule;
-parsInt(...) converti en entier;
-Si pas un chiffre passé en string, retourne NaN; */
 
 if (!isNaN(number)) {
   console.log('My number: ' + number);
