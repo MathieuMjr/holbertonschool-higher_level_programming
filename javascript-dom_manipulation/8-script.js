b7f43db9fd497cc3348f5de25974f3322ca9b493
Mathieu <mathieu.mjr@gmail.com>
2025-11-12 12:51:42 +0100

    say helllow

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,10 @@
+let hello = '';
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      const helloElem = document.querySelector('#hello');
+      helloElem.textContent = data.hello;
+    });
+});
